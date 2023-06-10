038932a5d7963df52577cacfbd945e3eadaf237e
Tiago Pedro Jr <tpj.tiago@gmail.com>
2023-06-09 22:44:47 -0300

    Add project files.

---
+++ ApiAuth/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
---
+++ ApiAuth/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApiAuth.Models;
+
+namespace ApiAuth.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "tiago", Password = "tiago", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "junior", Password = "junior", Role = "employee" });
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
